6ac79ec8acf5017e24eba8fb760d11e3f4c4e168
nurulalyh <nurulaliyah2303@gmail.com>
2023-08-20 12:22:21 +0800

    Add Tugas Eksplorasi

--- 02_Basic Programming/praktikum/Eksplorasi/main.go
+++ 02_Basic Programming/praktikum/Eksplorasi/main.go
@@ -1,7 +1,47 @@
 package main
 
-// import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func Palindrom(a string) bool {
+	a = strings.ToLower(a)
+	for i := 0; i < len(a)/2; i++ {
+		if a[i] != a[len(a)-1-i] {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-
+	var kata string
+	var tryAgain string
+	
+	for {
+		fmt.Println("\t\tProgram Mengecek Palindrom 1 Kata")
+		fmt.Println("----------------------------------------------------------------")
+		
+		fmt.Print("Masukkan satu kata tanpa spasi: ")
+		fmt.Scanln(&kata)
+		fmt.Println("----------------------------------------------------------------")
+		
+		
+		if Palindrom(kata) {
+			fmt.Printf("%s adalah palindrom.\n", kata)
+		} else {
+			fmt.Printf("%s bukan palindrom.\n", kata)
+		}
+		fmt.Println("----------------------------------------------------------------")
+		
+		fmt.Print("Apakah Anda ingin mencoba lagi? (y/n): ")
+		fmt.Scanln(&tryAgain)
+		fmt.Println("----------------------------------------------------------------")
+		if tryAgain != "y" && tryAgain != "Y" {
+			fmt.Println("Terima Kasih Telah Menggunakan Program Ini 😊")
+			fmt.Println("----------------------------------------------------------------")
+			break
+		}
+	}
 }
